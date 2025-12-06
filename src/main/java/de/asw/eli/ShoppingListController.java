80bbc24e55adb58a1df981c385ed52743024794b
justin <walter1justin1@gmail.com>
2025-12-06 10:21:58 +0100

    Reiinitialized Project with Thymeleaf included

---
+++ src/main/java/de/asw/eli/ShoppingListController.java
@@ -0,0 +1,15 @@
+package de.asw.eli;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ShoppingListController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello from the controller!");
+        return "index";
+    }
+}
--- src/test/java/de/asw/ELI/EliApplicationTests.java
+++ src/test/java/de/asw/ELI/EliApplicationTests.java
@@ -1,4 +1,4 @@
-package de.asw.ELI;
+package de.asw.eli;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
